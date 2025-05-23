080a80e2baf9d6edc6f6158fb74be1f625fffdbb
Omkar <omkarkendale9@gmail.com>
2025-05-22 19:33:55 -0500

    remove pycache stuff from git

--- main.py
+++ main.py
@@ -4,22 +4,18 @@ from dotenv import load_dotenv
 import os
 import chess
 import similar_positions_eval
-import frequency_squares
-import helpers
 
 load_dotenv()  
 TOKEN = os.getenv("LICHESS_TOKEN")
-PLAYER = os.getenv("PLAYER")
 
 session = berserk.TokenSession(TOKEN)
 client = berserk.Client(session=session)
 
-num_games = 500 #Number of games to import 
+num_games = 10 #Number of games to import 
 perf_types = "ultraBullet,bullet,blitz,rapid,classical,correspondence"
-games = client.games.export_by_player(PLAYER, max=num_games, perf_type=perf_types)
+games = client.games.export_by_player("omkar109", max=num_games, perf_type=perf_types)
 
-helpers.create_position_database(games)
+similar_positions_eval.create_position_database(games)
 test_pos = chess.Board()
 test_pos.set_fen("rn1q2k1/p1Bp1rpp/5n2/7P/3pb3/8/PP3PP1/2RQ1RK1 b KQkq - 0 1")
 similar_positions_eval.material_eval(test_pos, chess.WHITE)
-frequency_squares.print_frequency_tables()
--- similar_positions_eval.py
+++ similar_positions_eval.py
@@ -1,11 +1,25 @@
 import chess
-from helpers import _get_material_value, _mpf, white_material_database, black_material_database
+from collections import defaultdict
 
 """
 This evaluation function takes in a current position and finds the most similar position among all games played. Then it outputs the next move played in that position.
 This move may or may not be a legal move in the input position. In this case, a backup eval function must be relied on or engine evaluation
 """
 
+"""
+The position database is a hashtable where the key is the position in FEN string format and the 
+# value is a list that stores the next move made and info about the position. White indicates in white's turn 
+# to move. Each string is formatted as follows: rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+#Format of value: [next move, white_material_value, black_material_value]
+next move is a string in san format
+"""
+white_position_database = {}
+black_position_database = {}
+
+
+#The keys will be the difference between value of white's material and value of black's material. The values will be a list of positions (could have multiple positions per key)
+white_material_database = defaultdict(list)
+black_material_database = defaultdict(list)
 
 """
 This function takes in a position and side to play and outputs either the move to be made in this position or 
@@ -15,6 +29,68 @@ def get_next_move(board, color):
     positions = material_eval(board, color)
     return
 
+"""
+This function takes in a list of games in SAN format, and stores each position from the game in the position 
+databases
+"""
+def create_position_database(games):
+
+    #Iterate throught all player games
+    for game in games:
+        moves = game["moves"] #Get the move in string SAN format. Ex: "e4 e5 Nc6"
+        white_to_play = True #Flag to know which database to store move in
+
+        board = chess.Board()
+
+        #Iterate over each move in the game
+        for move in moves.split():
+            white_material_value, black_material_value = _get_material_value(board)
+            if white_to_play:
+                white_position_database[board.fen()] = [move, white_material_value, black_material_value]
+                white_material_database[_mpf(white_material_value, black_material_value)].append(board.fen()) 
+            else:
+                black_position_database[board.fen()] = [move, white_material_value, black_material_value]
+                black_material_database[_mpf(white_material_value, black_material_value)].append(board.fen()) 
+            white_to_play = not white_to_play
+            board.push(board.parse_san(move))
+        board.clear()
+    return
+
+"""
+Given a board object, returns the total value of white's material and of black's material
+"""        
+def _get_material_value(board):
+    piece_set = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING]
+    white_material_value = 0
+    black_material_value = 0
+    for piece in piece_set:
+        curr_bitboard = tuple(board.pieces(piece, chess.WHITE))
+        if piece == chess.PAWN:
+            white_material_value += len(curr_bitboard)
+        elif piece == chess.KNIGHT:
+            white_material_value += len(curr_bitboard) * 3
+        elif piece == chess.BISHOP:
+            white_material_value += len(curr_bitboard) * 3
+        elif piece == chess.ROOK:
+            white_material_value += len(curr_bitboard) * 5
+        elif piece == chess.QUEEN:
+            white_material_value += len(curr_bitboard) * 9
+
+    for piece in piece_set:
+        curr_bitboard = tuple(board.pieces(piece, chess.BLACK))
+        if piece == chess.PAWN:
+            black_material_value += len(curr_bitboard)
+        elif piece == chess.KNIGHT:
+            black_material_value += len(curr_bitboard) * 3
+        elif piece == chess.BISHOP:
+            black_material_value += len(curr_bitboard) * 3
+        elif piece == chess.ROOK:
+            black_material_value += len(curr_bitboard) * 5
+        elif piece == chess.QUEEN:
+            black_material_value += len(curr_bitboard) * 9
+    
+    return white_material_value, black_material_value
+    
 
 
 """
@@ -57,7 +133,9 @@ def material_eval(board, color):
 
 #Parameters to tune include, number of games to parse, weighting between heuristics, 
 
-
+#Hashing function given white and black material values (helps control number of entries filters by material_eval filter)
+def _mpf(white, black):
+    return (white+black)//white
 
 
 
